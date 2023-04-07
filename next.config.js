aee2d2bff00d3a7ef2dce07af67b08729503be2a
hue <badeagle85@gmail.com>
2023-04-07 20:57:14 +0900

    test

--- next.config.js
+++ next.config.js
@@ -1,19 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: '',
   reactStrictMode: true,
   images: {
     domains: ['product-image.kurly.com', 'localhost', 'shopping-phinf.pstatic.net', 'simg.ssgcdn.com', 'images.unsplash.com'],
   },
-  // images: {
-  //   remotePatterns: [
-  //     {
-  //       protocol: 'https',
-  //       hostname: 'product-image.kurly.com',
-  //       port: '',
-  //       // pathname: '/account123/**',
-  //     },
-  //   ],
-  // },
 };
 
 module.exports = nextConfig;
